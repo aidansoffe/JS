508081f26f6ccb7233428a06becfbab0a2d8e14b
Aidan Munarbekova <42157883+aidansoffe@users.noreply.github.com>
2019-05-01 22:04:38 -0500

    Create getNthElement.js

---
+++ getNthElement.js
@@ -0,0 +1,9 @@
+function getNthElement(array, penguin) {
+  if(array.length === 0){
+    return undefined;
+  }else{
+    return array[penguin];
+  }
+}
+// var numbers = [1, 2, 3];
+// console.log(numbers[1])
