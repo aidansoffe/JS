35f900c3f7edf7427ddc029dae34f86bb9f69378
Aidan Munarbekova <42157883+aidansoffe@users.noreply.github.com>
2019-05-01 22:49:20 -0500

    Create computeTripledAreaOfARectangle.js

---
+++ computeTripledAreaOfARectangle.js
@@ -0,0 +1,10 @@
+function computeTripledAreaOfARectangle(length, width) {
+  const area = 3 * (length * width);
+  return area;
+}
+
+
+/**function computeTripledAreaOfARectangle(length, width) {
+  const area = length * width;
+  return area*3;
+} **/
