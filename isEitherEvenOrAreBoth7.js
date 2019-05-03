36ec41ed2bb045867b8e4e760ee305f7bde0b660
Aidan Munarbekova <42157883+aidansoffe@users.noreply.github.com>
2019-05-02 20:29:28 -0500

    Create isEitherEvenOrAreBoth7.js

---
+++ isEitherEvenOrAreBoth7.js
@@ -0,0 +1,9 @@
+function isEitherEvenOrAreBoth7(num1, num2) {
+  if (num1 %2===0 || num2 %2===0){
+    return true;
+  }else if (num1 ===7 && num2 === 7){
+    return true;
+  }else{
+    return false;
+  }
+}
