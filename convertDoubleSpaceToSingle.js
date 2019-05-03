50bf5892001f868e02f46be692e7f49362a0bb2e
Aidan Munarbekova <42157883+aidansoffe@users.noreply.github.com>
2019-05-03 17:20:47 -0500

    Create convertDoubleSpaceToSingle.js

---
+++ convertDoubleSpaceToSingle.js
@@ -0,0 +1,11 @@
+function convertDoubleSpaceToSingle(str) {
+  let output = [];
+  str = str.split('  ')
+  for ( var i =0; i < str.length; i++){
+    output.push(str[i]);
+  }
+  return output.join(' ');
+}
+
+var output = convertDoubleSpaceToSingle("string  with  double  spaces");
+console.log(output);
