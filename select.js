6e826f5d89f37458a8172df8af2aa58d21c84492
Aidan Munarbekova <42157883+aidansoffe@users.noreply.github.com>
2019-05-04 00:27:46 -0500

    Create select.js

---
+++ select.js
@@ -0,0 +1,19 @@
+function select(arr, obj) {
+  var output = {};
+  for (var letter of arr){
+    if(obj.hasOwnProperty(letter)){
+      output[letter] = obj[letter];
+    }
+  }
+  return output;
+}
+
+var arr = ['a', 'c', 'e'];
+var obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4
+};
+var out = select(arr, obj);
+console.log(out);
