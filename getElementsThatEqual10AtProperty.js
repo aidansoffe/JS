d064bf7cb03d0c60e68699620f5910ff74df00a3
Aidan Munarbekova <42157883+aidansoffe@users.noreply.github.com>
2019-05-05 17:38:30 -0500

    Create getElementsThatEqual10AtProperty.js

---
+++ getElementsThatEqual10AtProperty.js
@@ -0,0 +1,26 @@
+function getElementsThatEqual10AtProperty(obj, key) {
+  let res = [];
+  if( !Array.isArray(obj[key]) || obj[key] === [] || (!obj[key])) {
+    return [];
+  }
+  for ( let key in obj){
+    if(obj[key].length){
+     obj[key].filter(numbers => {
+      if(numbers === 10){
+        res.push(numbers)
+      }
+     })
+    } 
+  }
+  return res;
+}
+
+var obj = {
+  key: [1000, 10, 50, 10, 10],
+  nt: [1],
+  gt: 10,
+  fr: 10,
+  se: 10
+};
+var output = getElementsThatEqual10AtProperty(obj, 'key');
+console.log(output);
