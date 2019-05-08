f89a081018030ca29f6b713cb9dbb93f4eba181e
Aidan Munarbekova <42157883+aidansoffe@users.noreply.github.com>
2019-05-08 15:51:38 -0500

    Create getSquaredElementsAtProperty.js

---
+++ getSquaredElementsAtProperty.js
@@ -0,0 +1,17 @@
+function getSquaredElementsAtProperty(obj, key) {
+  var output = [];
+  if(!Array.isArray(obj[key]) || obj[key].length < 1){
+    return [];
+  }else{
+    for (var prop in obj[key]){
+      output.push( obj[key][prop] * obj[key][prop] );
+    }
+  }
+  return(output);
+}
+var obj = {
+  key: [2, 1, 5]
+};
+getSquaredElementsAtProperty(obj, 'key');
+
+=> [ 4, 1, 25 ]
