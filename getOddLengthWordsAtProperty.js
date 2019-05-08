3699cc6cb05816040009aaa48ea69dc484051fb0
Aidan Munarbekova <42157883+aidansoffe@users.noreply.github.com>
2019-05-08 13:30:24 -0500

    Create getOddLengthWordsAtProperty.js

---
+++ getOddLengthWordsAtProperty.js
@@ -0,0 +1,19 @@
+function getOddLengthWordsAtProperty(obj, key) {
+  var output = [];
+ if(!Array.isArray(obj[key])){
+   return [];
+ }else 
+for (value in obj[key]){
+  if(obj[key][value].length %2 !== 0){
+    output.push(obj[key][value]);
+  }
+}
+return output;
+}
+var obj = {
+  key: ['It', 'has', 'some', 'words']
+};
+getOddLengthWordsAtProperty(obj, 'key');
+
+=> [ 'has', 'words' ]
+   
