25dacea1876e73d89008dd0c5a6150dc808fdebd
Aidan Munarbekova <42157883+aidansoffe@users.noreply.github.com>
2019-05-10 23:45:11 -0500

    Create getSumOfAllElementsAtProperty.js

---
+++ getSumOfAllElementsAtProperty.js
@@ -0,0 +1,17 @@
+function getSumOfAllElementsAtProperty(obj, key) {
+  var output = 0;
+  if(!Array.isArray(obj[key]) || obj[key].length ===0){
+    return 0;
+  }else{
+  for(var number in obj[key]){
+    output=output+obj[key][number];
+  }
+  }
+  return output;
+}
+var obj = {
+  key: [4, 1, 8]
+};
+getSumOfAllElementsAtProperty(obj, 'key');
+
+=> 13
