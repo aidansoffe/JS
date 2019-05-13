e856e0427df0f6637af3a1eef694383ee3d0f6b8
Aidan Munarbekova <42157883+aidansoffe@users.noreply.github.com>
2019-05-12 22:51:30 -0500

    Update findSmallestNumberAmongMixedElements.js

--- findSmallestNumberAmongMixedElements.js
+++ findSmallestNumberAmongMixedElements.js
@@ -1,13 +1,17 @@
 function findSmallestNumberAmongMixedElements(arr) {
   if(arr && arr.length ){
-    var filteredNums = arr.filter(function(item){
+    let filteredNums = arr.filter(function(item){
       return typeof item === 'number';
-    })
-     var minNum = Math.min(...filteredNums);
-     return minNum;
-  } else {
-    return 0;
+    });
+    if (filteredNums.length === 0){
+      return 0;
+    }
+    
+   let minNum = Math.min(...filteredNums);
+   return minNum;
   }
+  
+  return 0;
 }
 
 findSmallestNumberAmongMixedElements([10, 6 , 8, 'hi', true, 2]);
