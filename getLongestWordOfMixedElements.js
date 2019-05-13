5a7e18e5f9875d2ef165bdfd4927fb19eec06c79
Aidan Munarbekova <42157883+aidansoffe@users.noreply.github.com>
2019-05-13 16:25:58 -0500

    Create getLongestWordOfMixedElements.js

---
+++ getLongestWordOfMixedElements.js
@@ -0,0 +1,25 @@
+function getLongestWordOfMixedElements(arr) {
+ if(arr.length<1){
+   return "";
+ }
+ var newArr = [];
+ for (var item in arr){
+   if(typeof arr[item] === 'string'){
+     newArr.push(arr[item]);
+   }
+ }
+ if(newArr.length<1){
+   return "";
+ }
+ var longestWord = newArr[0];
+ for (var i in newArr){
+   if(longestWord.length<newArr[i].length){
+     longestWord=newArr[i];
+   }
+ }
+ return longestWord;
+
+}
+getLongestWordOfMixedElements([3, 'word', 5, 'up', 3, 1]);
+
+=> 'word'
