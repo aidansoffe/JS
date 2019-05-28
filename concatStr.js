0f00cbe96cef1e0bef35df5406e7f5327cb5f58c
Aidan Munarbekova <42157883+aidansoffe@users.noreply.github.com>
2019-05-27 21:14:18 -0500

    Create concatStr.js

---
+++ concatStr.js
@@ -0,0 +1,13 @@
+var str1 = 'Aidan';
+var str2 = 'Beck';
+
+function concatStr(str1){
+var str3 = str1 + ' and ' + str2;
+return str3 + ' = the length of the string is: '+ str3.length
+}
+
+
+concatStr(str1)
+
+
+=> 'Aidan and Beck = the length of the string is: 14'
