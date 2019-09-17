12a70591fdf30b508fa837571bef4461fd92ce4f
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-09-16 22:10:56 -0400

    Create beatles.js

---
+++ beatles.js
@@ -0,0 +1,41 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var result = []
+
+  for (var i = 0; i < musicians.length; i++){
+     result.push(musicians[i] + ' ' + "plays" + ' '+ instruments[i])
+}
+return result
+}
+
+
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts){
+  var n = []
+  for (var i=0; i<facts.length; i++){
+    n.push(facts[i] + "!!!")
+  }
+  return n
+}
+
+
+const n = 9;
+
+function iLoveTheBeatles(n){
+  var k = []
+  
+  do {
+  k = "I love the Beatles!"
+  n++
+}
+while (n > 15);
+}
