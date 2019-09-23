d5b484a27e15f6d60a6dbe898c9c28069ffe2b2a
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-09-23 13:41:20 -0400

    Rename squareElements.js to squareElement.js

---
+++ squareElement.js
@@ -0,0 +1,12 @@
+function squareElements(arr) {
+    var newArr = []
+for(var i=0;i<arr.length; i++){
+    newArr.push(arr[i]*arr[i])
+}
+return newArr
+}
+
+function squareElements(arr){
+let result = arr.map(x => x ** 2)
+return result
+}
