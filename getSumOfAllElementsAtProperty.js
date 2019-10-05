5e6d92e893950cae42be9b917218a9f464c6e014
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-10-05 04:16:21 -0400

    Update getSumOfAllElementsAtProperty.js

--- getSumOfAllElementsAtProperty.js
+++ getSumOfAllElementsAtProperty.js
@@ -15,3 +15,12 @@ var obj = {
 getSumOfAllElementsAtProperty(obj, 'key');
 
 => 13
+
+///////////
+
+function getSumOfAllElementsAtProperty(obj, key) {
+  if(!Array.isArray(obj[key]) || obj[key].length === 0 ){
+      return 0
+  }
+  return obj[key].reduce((a,b) => a+b)
+}
