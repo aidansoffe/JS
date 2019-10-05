2b0badc9d10cc6bbe748aa1a33d97d918c386e0c
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-10-05 03:51:23 -0400

    Update getProductOfAllElementsAtProperty.js

--- getProductOfAllElementsAtProperty.js
+++ getProductOfAllElementsAtProperty.js
@@ -16,3 +16,14 @@ var obj = {
 getProductOfAllElementsAtProperty(obj, 'key');
 
 => 24
+
+
+///////////////
+
+function getProductOfAllElementsAtProperty(obj, key) {
+  // your code here
+  if(!Array.isArray(obj[key]) || obj[key].length === 0){
+      return 0
+  }
+  return obj[key].reduce((a, b) => a*b, 1)
+}
