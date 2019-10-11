3a828518acac663f37ea90d683ae697643c9f563
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-10-10 23:27:05 -0400

    Update multiply.js

--- multiply.js
+++ multiply.js
@@ -8,3 +8,23 @@ multiply(9, 5);
 
 
 9/(1/5)= 9/0.2= 45
+
+///////////////
+
+
+   var resultIsPositive = true;
+    if((num1 > 0 && num2 < 0) || (num2 > 0 && num1 < 0) ){
+        resultIsPositive = false
+    }
+    num1 = Math.abs(num1);
+    num2 = Math.abs(num2);
+    
+var result = 0
+for(var i=0; i < num2; i++ ){
+    result += num1 
+}
+    if(resultIsPositive){
+        return result;
+    }else{
+        return -result
+    }
