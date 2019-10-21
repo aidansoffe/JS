17f047e8a084d316a68d26ecd7387e91bea5c897
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-10-21 19:12:24 -0400

    Create haiku.js

---
+++ haiku.js
@@ -0,0 +1,7 @@
+function generateHaiku(firstLine, secondLine, thirdLine) {
+  // create a haiku variable
+  // assign it to an expression involving the input lines, such that they format correct when the return value is logged to the console
+  // return the haiku variable
+  var haiku = firstLine+'\n' + secondLine+'\n' + thirdLine
+  return haiku
+}
