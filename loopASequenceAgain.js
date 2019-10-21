db0266f2fb54137e8a2df75a5632ba35680da87b
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-10-21 01:37:47 -0400

    Create loopASequenceAgain.js

---
+++ loopASequenceAgain.js
@@ -0,0 +1,8 @@
+function loopASequenceAgain(start, end) {
+  // create a loop which loops from start to end
+    // log current value to console
+    for(var start = start;start <= end; start++){
+        console.log(start)
+        }
+
+}
