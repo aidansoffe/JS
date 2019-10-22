0b0922508519f834a232b4cba276f32b5d05207c
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-10-22 17:43:54 -0400

    Create loopInReverse.js

---
+++ loopInReverse.js
@@ -0,0 +1,7 @@
+function loopInReverse(array) {
+  // create a loop which iterates from back to front of the input array
+    // log current array element to the console
+    for(var i = array.length-1; i>-1; i--){
+        console.log(array[i])
+    }
+}
