aae634c15758929e86bbd568871a484ea4ff784d
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-10-22 18:22:04 -0400

    Create loopAString.js

---
+++ loopAString.js
@@ -0,0 +1,12 @@
+function loopAString(string) {
+  // create an index variable
+  // create a loop which iterates over the input string
+    // log current string character to the console
+    // increment value of index variable
+      var index = 0
+    while(index < string.length){
+      
+        console.log(string[index])
+          index++
+    }
+}
