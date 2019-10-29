5ee508a9e5aef891a6471e12888f71d26d23e8ec
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-10-29 00:27:27 -0400

    Update addToFron.js

--- addToFron.js
+++ addToFron.js
@@ -3,3 +3,10 @@ function addToFront(arr, element) {
 }
 
 // ...arr   if you have array inside the array.
+
+
+function addToFront(arr, element) {
+  arr.unshift(element)
+    return arr;  
+}
+///It should be the SAME array, not a new array.
