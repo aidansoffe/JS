31cfc0f98101cf9a4c220cb4fea4eb6c0cc81111
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2020-02-09 14:23:20 -0500

    Create theBeatlesPlay.js

---
+++ theBeatlesPlay.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musicians, instruments) {
+  let newArray = []
+  for ( let i = 0; i < musicians.length ; i++){
+      newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+    }
+  return newArray
+  
+}
+
+function johnLennonFacts(facts) {
+ let newArray = []
+ var i = 0
+ while ( i < facts.length ){
+   newArray.push(facts[i] + " !!!")
+   i++
+ }
+ return newArray
+}
+
+function  iLoveTheBeatles(num) {
+let newArray = []
+
+do {
+  newArray.push("I love the Beatles!")
+  num++
+} while(num < 15 );
+
+return newArray
+}
