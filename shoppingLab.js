2039ef1aecd8e3394e78f3f0910450f46e4d77c4
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2020-02-11 23:49:16 -0500

    Create shoppingLab.js

---
+++ shoppingLab.js
@@ -0,0 +1,58 @@
+var cart = [];
+
+function getCart() {
+ return cart;
+}
+
+function setCart(c) {
+  cart = c;
+  return cart;
+}
+
+function addToCart(item) {
+
+ var obj = {}
+ obj.itemName = item
+ obj.itemPrice = Math.floor(Math.random() * 100) + 1
+ 
+ cart.push(obj)
+ return `${item} has been added to your cart.`
+}
+
+
+function viewCart(c) {
+  let l = c.length
+  if (!l) {
+    return 'Your shopping cart is empty.';
+  }
+
+  var newString = 'In your cart, you have ';
+
+  var mapping = c.map(function (item, index) {
+    let stuff = `${item.itemName} at $${item.itemPrice}, `
+    let lastItem = `and ${item.itemName} at $${item.itemPrice}.`
+    let oneItem = `${item.itemName} at $${item.itemPrice}.`
+    if( c.length === 1) {
+    
+        newString = newString.concat(oneItem)
+        return newString
+      }
+     else if(index === c.length-1) {
+           newString = newString.concat(lastItem)
+      } else {
+            newString = newString.concat(stuff)
+      }
+   });
+  return newString;
+}
+  
+
+
+
+function removeFromCart(item) {
+  // write your code here
+}
+
+function placeOrder(cardNumber) {
+  // write your code here
+}
